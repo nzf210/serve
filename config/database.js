e757530f59365878f64b496f98f77073a4517a6b
nzf210 <syahril.symd@gmail.com>
2022-05-18 11:36:54 +0800

    edit env

--- config/database.js
+++ config/database.js
@@ -2,14 +2,18 @@ import { Sequelize } from "sequelize";
 
 let usr = 'postgres'
 let pass = 'qwerty'
+let host = '127.0.0.1'
+let dbnm = 'app'
 if (process.env === 'production') {
-    usr = process.env.DBUSER,
-        pass = process.env.DBPASS
+    usr = process.env.DBUSER
+    pass = process.env.DBPASS
+    host = process.env.DBHOST
+    dbnm = process.env.DBNM
 }
 
-const db = new Sequelize('app', usr, pass,
+const db = new Sequelize(dbnm, usr, pass,
     {
-        host: "127.0.0.1",
+        host: host,
         dialect: "postgres", port: 5432
     }
 );
