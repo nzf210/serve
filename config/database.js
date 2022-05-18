4b5c11b447f1c5987117c8df1fa7982515caf072
nzf210 <syahril.symd@gmail.com>
2022-05-18 22:53:24 +0800

    update git

--- config/database.js
+++ config/database.js
@@ -6,18 +6,21 @@ let usr = 'postgres'
 let pass = 'qwerty'
 let host = '127.0.0.1'
 let dbnm = 'app'
-console.log('env', process.title)
-if (process.title !== 'node') {
+console.log('env ===', process.title, process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'production') {
     usr = process.env.DBUSER
     pass = process.env.DBPASS
     host = process.env.DBHOST
-    dbnm = process.env.DBNM
+    dbnm = process.env.BDNM
 }
 
 const db = new Sequelize(dbnm, usr, pass,
     {
         host: host,
-        dialect: "postgres", port: 5432
+        dialect: "postgres", port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 );
 
