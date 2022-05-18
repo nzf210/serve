65ad94491affef0f865d4c1af25f6f50dd6e2ed5
nzf210 <syahril.symd@gmail.com>
2022-05-18 14:21:50 +0800

    update vercel

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ let pass = 'qwerty'
 let host = '127.0.0.1'
 let dbnm = 'app'
 console.log('env', process.title)
-if (process.env === 'production') {
+if (process.title === 'node') {
     usr = process.env.DBUSER
     pass = process.env.DBPASS
     host = process.env.DBHOST
