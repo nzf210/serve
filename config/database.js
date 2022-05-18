f74f3453ca37f6bc13d5bb1a3730930fc11f00f3
nzf210 <syahril.symd@gmail.com>
2022-05-18 12:12:23 +0800

    update Config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 let usr = 'postgres'
 let pass = 'qwerty'
 let host = '127.0.0.1'
 let dbnm = 'app'
-if (process.env === 'production') {
+if (process.env !== 'production') {
     usr = process.env.DBUSER
     pass = process.env.DBPASS
     host = process.env.DBHOST
